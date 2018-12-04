7f40aed52c5c3c0e5c859255505d5574cdd636bd
myer0432 <smyers@ou.edu>
2018-12-04 17:09:05 -0600

    begun model
    reworked data processing to use resized images plus some logic changes

--- Code/data.py
+++ Code/data.py
@@ -8,66 +8,43 @@ from random import shuffle
 class data:
     def __init__(self, csv_path, target_feature, img_path):
         # Load data
+        print("Loading data...")
         bee_csv, bee_imgs = self.load_data(csv_path, img_path)
-        bee_imgs = self.pad_data(bee_imgs)
-        # Scale image values to a range of 0 to 1
-        bee_imgs = bee_imgs / 255.0
-        # plt.imshow(bee_final_imgs[0])
-        # plt.show()
         # Get just species targets
         bee_targets = bee_csv[:,target_feature]
         # Get classes
         self.get_classes(bee_targets)
-        # for target in bee_targets:
-        #     target = self.classes.index(target)
         # Split data
         self.split(bee_imgs, bee_targets)
 
     def load_data(self, csv_path, img_path):
-        print("Loading data...")
+        # Load targets (bee classification data)
         bee_csv = pd.read_csv(csv_path) # Read data csv
         bee_csv = bee_csv.values # Convert to numpy array
         bee_csv = bee_csv[np.argsort(bee_csv[:, 0])[::-1]] # Sort by first column = name of image
-        filenames = os.listdir(img_path) # Get image paths
-        bee_data = np.empty((len(filenames)), dtype=np.object)
-        for i in range(len(filenames)):
-            bee_data[i] = np.append(bee_csv[i], imageio.imread(img_path+"/"+filenames[i]))
-        shuffle(bee_data)
-        return bee_data, bee_data[:,-1]
-
-    def pad_data(self, bee_imgs):
-        print("Padding...")
-        largest_first_dim = 0
-        largest_second_dim = 0
-        for i in range(bee_imgs.shape[0]):
-            if bee_imgs[i].shape[0] > largest_first_dim:
-                largest_first_dim = bee_imgs[i].shape[0]
-            if bee_imgs[i].shape[1] > largest_second_dim:
-                largest_second_dim = bee_imgs[i].shape[1]
-        for i in range(bee_imgs.shape[0]):
-            first_dim_tuple = (0, largest_first_dim-bee_imgs[i].shape[0])
-            second_dim_tuple = (0, largest_second_dim-bee_imgs[i].shape[1])
-            third_dim_tuple = (0,0)
-            bee_imgs[i] = np.pad(bee_imgs[i], (first_dim_tuple, second_dim_tuple, \
-    third_dim_tuple), 'constant', constant_values=0)
-        return bee_imgs
+        shuffle(bee_csv) # Shuffle data
+        # Load predictor data (images)
+        bee_imgs = np.empty(len(bee_csv), dtype=object)
+        for i in range(len(bee_csv)):
+            bee_imgs[i] = imageio.imread(img_path+"/"+bee_csv[i,0]) # First item in bees is image name
+        return bee_csv, bee_imgs
 
     def get_classes(self, bee_targets):
+        # Get unique types of species
         self.classes = []
         for item in bee_targets:
             if not item in self.classes:
                 self.classes.append(item)
+        print("Data classes found:", self.classes)
 
     def split(self, bee_imgs, bee_targets):
+        # Split data into 60% training, 20% validation, and 20% testing
         print("Splitting data...")
-        # Split data into 64% training, 16% validation, and 20% testing
-        data = bee_imgs[:int(len(bee_imgs)*.8)]
-        self.training_data = data[:int(len(data)*.8)]
-        self.validation_data = data[int(len(data)*.8):]
+        self.training_data = bee_imgs[:int(len(bee_imgs)*.6)]
+        self.validation_data = bee_imgs[int(len(bee_imgs)*.6):int(len(bee_imgs)*.8)]
         self.testing_data = bee_imgs[int(len(bee_imgs)*.8):]
-        targets = bee_targets[:int(len(bee_targets)*.8)]
-        self.training_targets = targets[:int(len(targets)*.8)]
-        self.validation_targets = targets[int(len(targets)*.8):]
+        self.training_targets = bee_targets[:int(len(bee_targets)*.6)]
+        self.validation_targets = bee_targets[int(len(bee_targets)*.6):int(len(bee_targets)*.8)]
         self.testing_targets = bee_targets[int(len(bee_targets)*.8):]
 
     def print(self):
--- Code/neural-net.py
+++ Code/neural-net.py
@@ -10,20 +10,23 @@ from data import data
 
 def make_model(bee_data):
     print("Training model...")
-    model = Sequential()
-    model.add(Flatten())
-    model.add(Dense(3, activation=tf.nn.relu))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(bee_data.training_data, bee_data.training_targets, epochs=150, batch_size=10)
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+    model.fit(bee_data.training_data, bee_data.training_targets, epochs=5)
     print("Scoring...")
     scores = model.evaluate(bee_data.validation_data, bee_data.validation_targets)
     print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 def main():
     # Read data
-    bee_data = data("../Data/bee_data.csv", 5, "../Data/bee_imgs") # Column 5 = Species
+    bee_data = data("../Data/bee_data.csv", 5, "../Data/resized_bee_imgs") # Column 5 = Species
     bee_data.print()
-    print(bee_data.training_data)
     make_model(bee_data)
 
 if __name__ == "__main__":
